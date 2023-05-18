2e0ec53d42ea1c4ca47a5717098a34a69785aa4c
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-05-18 20:53:25 -0300

    finished autheticacion on frontend

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,19 +1,21 @@
+import { cookies } from 'next/headers'
 import { Copyright } from './components/Copyright'
 import { SignIn } from './components/Signin'
 import { Hero } from './components/Hero'
 import { EmptyMemories } from './components/EmptyMemories'
+import { Profile } from './components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
-      {/* left */}
-
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
 
         {/* Stripes */}
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
