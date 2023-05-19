db5e8d7b73f13274e0ddd748cd3aa6b9a7485961
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-05-19 16:02:55 -0300

    added memory routes for mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,26 +1,62 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { useRouter } from 'expo-router'
 
+import { api } from '../src/lib/api'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import * as SecureStore from 'expo-secure-store'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-import blurBg from './src/assets/blur.png'
-import Stripes from './src/assets/stripes.svg'
+import blurBg from '../src/assets/blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { styled } from 'nativewind'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+import { useEffect } from 'react'
 
 const StyleStripes = styled(Stripes)
-
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/7b23b76dd650ddc3e54f',
+}
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+  const [request, response, siginWithGithub] = useAuthRequest(
+    {
+      clientId: '7b23b76dd650ddc3e54f',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+  async function handleGithubOauthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOauthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -44,6 +80,7 @@ export default function App() {
           </Text>
         </View>
         <TouchableOpacity
+          onPress={() => siginWithGithub()}
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
         >
